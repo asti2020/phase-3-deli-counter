4adc2e01f7fe2558a5e7abd3630c07f35b5d899e
Astika Luintel <astild2020@gmail.com>
2022-10-11 12:15:20 -0400

    commit tree 5a53a0c11f2c9a0e5a4ac14a878ab9dbdb15c060

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli = [1, 2, 3, 4]
+def line array
+    if array == []
+    puts "The line is currently empty."
+
+    elsif array.size == 1
+
+        puts "your position is" + array += 1
+
+    else 
+        puts "Your current place in the lineis #{array.size}!"
+    end
+end
+
+line(katz_deli)
+
+
+def take_a_number
+
+end
